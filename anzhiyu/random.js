c8c658a1f130aca704709712e7f98cb38cbff17b
hg <hg6696@qq.com>
2026-02-19 09:02:23 +0800

    Site updated: 2026-02-19 09:02:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/02/17/hello-world/"];function toRandomPost(){
+var posts=["2026/02/18/2026年AI大模型爆发：从千问3-5到GPT-5-2，谁将引领AGI时代/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
